256e7a1ed59822b89894f52d18cdfe98a1a72ec6
anthonymjimenez <anthonyjim96@gmail.com>
2020-09-15 12:40:02 -0400

    commiting lab

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end 
+    def breed=(dog)
+        @breed = dog
+    end
+
+    def breed
+        @breed
+    end
+end
